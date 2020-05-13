8ae8e0742aaea10171fa1ce1bf7a572f40c81970
Ubuntu <ubuntu@ip-172-31-20-232.ec2.internal>
2020-05-13 17:50:13 +0000

    Add Start.sh

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CONSENSUS="$1"
+
+if [ $CONSENSUS = "poet" ]; then
+    docker-compose -f poet.yaml up -d && docker-compose -f dev.yaml up -d 
+fi;
+
+if [ $CONSENSUS = "devmode" ]; then
+    docker-compose -f default.yaml up -d && docker-compose -f dev.yaml up -d 
+fi;

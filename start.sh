e6f262532ca13fbdac2b6689fb426a16e7ef0603
MOhammadreza <mohammadreza.rasolroveicy@polymtl.ca>
2020-05-15 15:10:23 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,45 +1,99 @@
 #!/bin/bash
 
 CONSENSUS="$1"
-VERSION="$2"
-
-if [ $CONSENSUS = "poet" ]; then
-    export CONFIG="sawtooth.consensus.algorithm.name=PoET \
-            sawtooth.consensus.algorithm.version=$VERSION \
-            sawtooth.poet.report_public_key_pem="$(cat * /etc/sawtooth/simulator_rk_pub.pem)" \
-            sawtooth.poet.valid_enclave_measurements=$(poet enclave measurement) \
-            sawtooth.poet.valid_enclave_basenames=$(poet enclave basename) \
-            sawtooth.poet.block_claim_delay=1 \
-            sawtooth.poet.key_block_claim_limit= 100000 \
-            sawtooth.poet.ztest_minimum_win_count=999999999"
-
-fi;
-
+CODE="bash -c \"\
+        sawadm keygen --force && \
+        mkdir -p /poet-shared/validator-0 || true && \
+        cp -a /etc/sawtooth/keys /poet-shared/validator-0/ && \
+        while [ ! -f /poet-shared/poet-enclave-measurement ]; do sleep 1; done && \
+        while [ ! -f /poet-shared/poet-enclave-basename ]; do sleep 1; done && \
+        while [ ! -f /poet-shared/poet.batch ]; do sleep 1; done && \
+        cp /poet-shared/poet.batch / && \
+        sawset genesis \
+          -k /etc/sawtooth/keys/validator.priv \
+          -o config-genesis.batch && \
+        sawset proposal create \
+          -k /etc/sawtooth/keys/validator.priv \
+          sawtooth.consensus.algorithm.name=PoET \
+          sawtooth.consensus.algorithm.version=0.1 \
+          sawtooth.poet.report_public_key_pem=\
+          \\\"$$(cat /poet-shared/simulator_rk_pub.pem)\\\" \
+          sawtooth.poet.valid_enclave_measurements=$$(cat /poet-shared/poet-enclave-measurement) \
+          sawtooth.poet.valid_enclave_basenames=$$(cat /poet-shared/poet-enclave-basename) \
+          -o config.batch && \
+        sawset proposal create \
+          -k /etc/sawtooth/keys/validator.priv \
+             sawtooth.poet.target_wait_time=5 \
+             sawtooth.poet.initial_wait_time=25 \
+             sawtooth.publisher.max_batches_per_block=100 \
+          -o poet-settings.batch && \
+        sawadm genesis \
+          config-genesis.batch config.batch poet.batch poet-settings.batch && \
+        sawtooth-validator -v \
+          --bind network:tcp://eth0:8800 \
+          --bind component:tcp://eth0:4004 \
+          --bind consensus:tcp://eth0:5050 \
+          --peering static \
+          --endpoint tcp://validator-0:8800 \
+          --scheduler parallel \
+          --network-auth trust
+    \""
 if [ $CONSENSUS = "pbft" ]; then
-    export CONFIG="sawtooth.consensus.algorithm.name=pbft \
-            sawtooth.consensus.algorithm.version=$VERSION \
-            sawtooth.consensus.pbft.members=[]"
-fi;
-
-if [ $CONSENSUS = "raft" ]; then
-    export CONFIG="sawtooth.consensus.algorithm.name=raft \
-            sawtooth.consensus.algorithm.version=$VERSION \
-            sawtooth.consensus.raft.peers=[]"
+   CODE="bash -c \"
+        if [ -e /pbft-shared/validators/validator-0.priv ]; then
+          cp /pbft-shared/validators/validator-0.pub /etc/sawtooth/keys/validator.pub
+          cp /pbft-shared/validators/validator-0.priv /etc/sawtooth/keys/validator.priv
+        fi &&
+        if [ ! -e /etc/sawtooth/keys/validator.priv ]; then
+          sawadm keygen
+          mkdir -p /pbft-shared/validators || true
+          cp /etc/sawtooth/keys/validator.pub /pbft-shared/validators/validator-0.pub
+          cp /etc/sawtooth/keys/validator.priv /pbft-shared/validators/validator-0.priv
+        fi &&
+        if [ ! -e config-genesis.batch ]; then
+          sawset genesis -k /etc/sawtooth/keys/validator.priv -o config-genesis.batch
+        fi &&
+        while [[ ! -f /pbft-shared/validators/validator-1.pub ]];
+        do sleep 1; done
+        echo sawtooth.consensus.pbft.members=\\['\"'$$(cat /pbft-shared/validators/validator-0.pub)'\"'\\] &&
+        if [ ! -e config.batch ]; then
+         sawset proposal create \
+            -k /etc/sawtooth/keys/validator.priv \
+            sawtooth.consensus.algorithm.name=pbft \
+            sawtooth.consensus.algorithm.version=1.0 \
+            sawtooth.consensus.pbft.members=\\['\"'$$(cat /pbft-shared/validators/validator-0.pub)'\"'\\] \
+            sawtooth.publisher.max_batches_per_block=1200 \
+            -o config.batch
+        fi &&
+        if [ ! -e /var/lib/sawtooth/genesis.batch ]; then
+          sawadm genesis config-genesis.batch config.batch
+        fi &&
+        if [ ! -e /root/.sawtooth/keys/my_key.priv ]; then
+          sawtooth keygen my_key
+        fi &&
+        sawtooth-validator -vv \
+          --endpoint tcp://validator-0:8800 \
+          --bind component:tcp://eth0:4004 \
+          --bind consensus:tcp://eth0:5050 \
+          --bind network:tcp://eth0:8800 \
+          --scheduler parallel \
+          --peering static \
+          --maximum-peer-connectivity 10000
+      \" "
 fi;
 
 if [ $CONSENSUS = "devmode" ]; then
-    export CONFIG="sawtooth.consensus.algorithm.name=Devmode \
-            sawtooth.consensus.algorithm.version=$VERSION "
+
+CODE="sawadm keygen \
+        sawtooth keygen my_key \
+        sawset genesis -k /root/.sawtooth/keys/my_key.priv \
+        sawset proposal create -k /root/.sawtooth/keys/my_key.priv \
+        sawtooth.consensus.algorithm.name=Devmode \
+        sawtooth.consensus.algorithm.version=0.1 \
+        -o config.batch \
+        sawadm genesis config-genesis.batch config.batch"       
 fi;
 
-export CMD="sawadm keygen
-          sawtooth keygen my_key
-          sawset genesis -k /root/.sawtooth/keys/my_key.priv
-          sawset proposal create -k /root/.sawtooth/keys/my_key.priv \
-          $CONFIG \
-            -o config.batch
-          sawadm genesis config-genesis.batch config.batch"
+export CMD=$CODE
 
-docker-compose up
-
-# echo $CMD 
+docker-compose up 

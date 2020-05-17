2aed565bc18a215ecd72421808278dd9540204cc
MOhammadreza <mohammadreza.rasolroveicy@polymtl.ca>
2020-05-17 18:17:32 -0400

    Update change.sh

--- change.sh
+++ change.sh
@@ -7,14 +7,13 @@ if [ $CONSENSUS = "devmode" ]; then
     docker exec sawtooth-validator-default-0 bash -c '          
 
 if [ ! -e /root/.sawtooth/keys/my_key.priv ]; then
-            sawadm keygen 
             sawtooth keygen my_key
             sawset genesis -k /root/.sawtooth/keys/my_key.priv
           fi &&
           sawset proposal create -k /root/.sawtooth/keys/my_key.priv \
             sawtooth.consensus.algorithm.name=Devmode \
             sawtooth.consensus.algorithm.version=0.1 \
-            --url http://rest-api-0:8008 \
+            --url http://rest-api-0:8008
         '
 fi;
 
@@ -22,14 +21,13 @@ if [ $CONSENSUS = "raft" ]; then
     docker exec sawtooth-validator bash -c '
 
         if [ ! -e /root/.sawtooth/keys/my_key.priv ]; then
-          sawadm keygen 
           sawtooth keygen my_key
           sawset genesis -k /root/.sawtooth/keys/my_key.priv
         fi &&
         sawset proposal create -k /root/.sawtooth/keys/my_key.priv \
           sawtooth.consensus.algorithm.name=raft \
           sawtooth.consensus.algorithm.version=0.1 \
-          sawtooth.consensus.raft.peers=[\""$(cat /etc/sawtooth/keys/validator.pub)\""] 
+          sawtooth.consensus.raft.peers=[\""$(cat /etc/sawtooth/keys/validator.pub)\""] \
           --url http://rest-api-0:8008 && \
         sawset proposal create -k /root/.sawtooth/keys/my_key.priv \
           --url http://rest-api-0:8008 \
@@ -51,6 +49,11 @@ if [ $CONSENSUS = "pbft" ]; then
           sawadm keygen 
           sawtooth keygen my_key
           sawset genesis -k /root/.sawtooth/keys/my_key.priv
+        fi &&
+        if [ ! -e /pbft-shared/validators/validator-0.pub ]; then
+          mkdir -p /pbft-shared/validators || true
+          cp /etc/sawtooth/keys/validator.pub /pbft-shared/validators/validator-0.pub
+          cp /etc/sawtooth/keys/validator.priv /pbft-shared/validators/validator-0.priv
         fi &&
         sawset proposal create -k /root/.sawtooth/keys/my_key.priv \
 

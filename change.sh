5a4251d84c8be8e7cc845e6480400ab949cf8b33
MOhammadreza <mohammadreza.rasolroveicy@polymtl.ca>
2020-05-16 23:51:36 -0400

    Update change.sh

--- change.sh
+++ change.sh
@@ -4,7 +4,6 @@ CONSENSUS="$1"
 
 if [ $CONSENSUS = "poet" ]; then
     docker exec sawtooth-validator bash -c '
-        rm /var/lib/sawtooth/genesis.batch && \
         sawadm keygen --force && \
         mkdir -p /poet-shared/validator-0 || true && \
         cp -a /etc/sawtooth/keys /poet-shared/validator-0/ && \
@@ -12,6 +11,9 @@ if [ $CONSENSUS = "poet" ]; then
         while [ ! -f /poet-shared/poet-enclave-basename ]; do sleep 1; done && \
         while [ ! -f /poet-shared/poet.batch ]; do sleep 1; done && \
         cp /poet-shared/poet.batch / && \
+        sawset genesis \
+          -k /etc/sawtooth/keys/validator.priv \
+          -o config-genesis.batch && \
         sawset proposal create \
           -k /etc/sawtooth/keys/validator.priv \
           sawtooth.consensus.algorithm.name=PoET \
@@ -25,19 +27,16 @@ if [ $CONSENSUS = "poet" ]; then
              sawtooth.poet.target_wait_time=5 \
              sawtooth.poet.initial_wait_time=25 \
              sawtooth.publisher.max_batches_per_block=100 \
-          -o poet-settings.batch && \
-        sawadm genesis \
-          config-genesis.batch config.batch poet.batch poet-settings.batch'
+          -o poet-settings.batch && \'
 fi;
 
 if [ $CONSENSUS = "devmode" ]; then
     docker exec sawtooth-validator bash -c '
-        rm /var/lib/sawtooth/genesis.batch && \
         sawadm keygen --force
         sawtooth keygen --force my_key
+        sawset genesis -k /root/.sawtooth/keys/my_key.priv
         sawset proposal create -k /root/.sawtooth/keys/my_key.priv \
         sawtooth.consensus.algorithm.name=Devmode \
         sawtooth.consensus.algorithm.version=0.1 \
-        -o config.batch
-        sawadm genesis config-genesis.batch config.batch'
+        -o config.batch'
 fi;

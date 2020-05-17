69fb437b2c4cf386f9cc754108c36a1f012b75e3
MOhammadreza <mohammadreza.rasolroveicy@polymtl.ca>
2020-05-16 23:49:49 -0400

    Update poet.yaml

--- change.sh
+++ change.sh
@@ -4,6 +4,7 @@ CONSENSUS="$1"
 
 if [ $CONSENSUS = "poet" ]; then
     docker exec sawtooth-validator bash -c '
+        rm /var/lib/sawtooth/genesis.batch && \
         sawadm keygen --force && \
         mkdir -p /poet-shared/validator-0 || true && \
         cp -a /etc/sawtooth/keys /poet-shared/validator-0/ && \
@@ -31,6 +32,7 @@ fi;
 
 if [ $CONSENSUS = "devmode" ]; then
     docker exec sawtooth-validator bash -c '
+        rm /var/lib/sawtooth/genesis.batch && \
         sawadm keygen --force
         sawtooth keygen --force my_key
         sawset proposal create -k /root/.sawtooth/keys/my_key.priv \

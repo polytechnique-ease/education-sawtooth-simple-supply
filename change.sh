c63334f5e042c3b650ade52f5924a0d7b1de4f06
MOhammadreza <mohammadreza.rasolroveicy@polymtl.ca>
2020-05-16 23:59:52 -0400

    Update change.sh

--- change.sh
+++ change.sh
@@ -3,7 +3,7 @@
 CONSENSUS="$1"
 
 if [ $CONSENSUS = "poet" ]; then
-    docker exec sawtooth-shell-default bash -c '
+    docker exec sawtooth-validator bash -c '
         sawadm keygen --force && \
         mkdir -p /poet-shared/validator-0 || true && \
         cp -a /etc/sawtooth/keys /poet-shared/validator-0/ && \
@@ -11,26 +11,31 @@ if [ $CONSENSUS = "poet" ]; then
         while [ ! -f /poet-shared/poet-enclave-basename ]; do sleep 1; done && \
         while [ ! -f /poet-shared/poet.batch ]; do sleep 1; done && \
         cp /poet-shared/poet.batch / && \
+        sawset genesis \
+          -k /etc/sawtooth/keys/validator.priv \
+          -o config-genesis.batch && \
         sawset proposal create \
           -k /etc/sawtooth/keys/validator.priv \
           sawtooth.consensus.algorithm.name=PoET \
           sawtooth.consensus.algorithm.version=0.1 \
-          sawtooth.poet.report_public_key_pem="$(cat /etc/sawtooth/simulator_rk_pub.pem)" \
-          sawtooth.poet.valid_enclave_measurements=$(poet enclave measurement) \
-          sawtooth.poet.valid_enclave_basenames=$(poet enclave basename) \
-          sawtooth.poet.block_claim_delay=1 \
-          sawtooth.poet.key_block_claim_limit= 100000 \
-          sawtooth.poet.ztest_minimum_win_count=999999999 \
+          sawtooth.poet.report_public_key_pem="$(cat /poet-shared/simulator_rk_pub.pem)" \
+          sawtooth.poet.valid_enclave_measurements=$(cat /poet-shared/poet-enclave-measurement) \
           -o config.batch && \
         sawset proposal create \
-         -k /etc/sawtooth/keys/validator.priv \
-          sawtooth.poet.target_wait_time=5 \
+          -k /etc/sawtooth/keys/validator.priv \
+             sawtooth.poet.target_wait_time=5 \
              sawtooth.poet.initial_wait_time=25 \
              sawtooth.publisher.max_batches_per_block=100 \
           -o poet-settings.batch 
           -o poet-settings.batch && \
         sawadm genesis \
-          config-genesis.batch config.batch poet.batch poet-settings.batch'
+          config-genesis.batch config.batch poet.batch poet-settings.batch && \
+        sawtooth-validator -v \
+          --peering static \
+          --endpoint tcp://validator-0:8800 \
+          --scheduler parallel \
+          --network-auth trust && \
+        rm /var/lib/sawtooth/genesis.batch'
 fi;
 
 if [ $CONSENSUS = "devmode" ]; then
